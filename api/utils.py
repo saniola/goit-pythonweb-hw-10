642d8911c951eed59954bd2bf0ddf072c410690d
Alex Kydanov <a.kidanov@gmail.com>
2024-12-15 11:22:33 +0200

    utils

--- api/utils.py
+++ api/utils.py
@@ -6,21 +6,22 @@ from database.db import get_db
 
 router = APIRouter(tags=["utils"])
 
+
 @router.get("/healthchecker")
 async def healthchecker(db: AsyncSession = Depends(get_db)):
-  try:
-    result = await db.execute(text("SELECT 1"))
-    result = result.scalar_one_or_none()
+    try:
+        result = await db.execute(text("SELECT 1"))
+        result = result.scalar_one_or_none()
 
-    if result is None:
-      raise HTTPException(
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        detail="Database is not configured correctly",
-      )
-    return {"message": "Welcome to FastAPI!"}
-  except Exception as e:
-    print(e)
-    raise HTTPException(
-      status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-      detail="Error connecting to the database",
-    )
+        if result is None:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Database is not configured correctly",
+            )
+        return {"message": "Welcome to FastAPI!"}
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error connecting to the database",
+        )
