5adc09712b8688e9eb49651bf2c3975a34d644fb
Alex Kydanov <a.kidanov@gmail.com>
2024-12-15 11:16:08 +0200

    format contacts

--- api/contacts.py
+++ api/contacts.py
@@ -11,79 +11,85 @@ from schemas import User
 
 router = APIRouter(prefix="/contacts", tags=["contacts"])
 
+
 @router.get("/", response_model=List[ContactResponse])
 async def read_contacts(
-  skip: int = 0,
-  limit: int = 100,
-  first_name: str | None = None,
-  last_name: str | None = None,
-  email: str | None = None,
-  db: AsyncSession = Depends(get_db),
-  user: User = Depends(get_current_user),
+    skip: int = 0,
+    limit: int = 100,
+    first_name: str | None = None,
+    last_name: str | None = None,
+    email: str | None = None,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user),
 ):
-  contact_service = ContactService(db)
-  contacts = await contact_service.search_contacts(skip, limit, first_name, last_name, email, user)
-  return contacts
+    contact_service = ContactService(db)
+    contacts = await contact_service.search_contacts(
+        skip, limit, first_name, last_name, email, user
+    )
+    return contacts
 
 
 @router.get("/{contact_id}", response_model=ContactResponse)
 async def read_contact(
-  contact_id: int,
-  db: AsyncSession = Depends(get_db),
-  user: User = Depends(get_current_user),
+    contact_id: int,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user),
 ):
-  contact_service = ContactService(db)
-  contact = await contact_service.get_contact(contact_id, user)
-  if contact is None:
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found"
-    )
-  return contact
+    contact_service = ContactService(db)
+    contact = await contact_service.get_contact(contact_id, user)
+    if contact is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found"
+        )
+    return contact
+
 
 @router.post("/", response_model=ContactResponse, status_code=status.HTTP_201_CREATED)
 async def create_contact(
-  body: ContactBase,
-  db: AsyncSession = Depends(get_db),
-  user: User = Depends(get_current_user),
+    body: ContactBase,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user),
 ):
-  contact_service = ContactService(db)
-  return await contact_service.create_contact(body, user)
+    contact_service = ContactService(db)
+    return await contact_service.create_contact(body, user)
+
 
 @router.put("/{contact_id}", response_model=ContactResponse)
 async def update_contact(
-  body: ContactBase,
-  contact_id: int,
-  db: AsyncSession = Depends(get_db),
-  user: User = Depends(get_current_user),
+    body: ContactBase,
+    contact_id: int,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user),
 ):
-  contact_service = ContactService(db)
-  contact = await contact_service.update_contact(contact_id, body, user)
-  if contact is None:
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found"
-    )
-  return contact
+    contact_service = ContactService(db)
+    contact = await contact_service.update_contact(contact_id, body, user)
+    if contact is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found"
+        )
+    return contact
+
 
 @router.delete("/{contact_id}", response_model=ContactResponse)
 async def remove_contact(
-  contact_id: int,
-  db: AsyncSession = Depends(get_db),
-  user: User = Depends(get_current_user),
+    contact_id: int,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user),
 ):
-  contact_service = ContactService(db)
-  contact = await contact_service.remove_contact(contact_id, user)
-  if contact is None:
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found"
-    )
-  return contact
+    contact_service = ContactService(db)
+    contact = await contact_service.remove_contact(contact_id, user)
+    if contact is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found"
+        )
+    return contact
+
 
 @router.get("/birthdays/", response_model=List[ContactResponse])
 async def upcoming_birthdays(
-  db: AsyncSession = Depends(get_db),
-  user: User = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user),
 ):
-  contact_service = ContactService(db)
-  contacts = await contact_service.get_upcoming_birthdays(user)
-  return contacts
-
+    contact_service = ContactService(db)
+    contacts = await contact_service.get_upcoming_birthdays(user)
+    return contacts
